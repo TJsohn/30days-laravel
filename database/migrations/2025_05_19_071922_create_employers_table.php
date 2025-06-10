268f5348363f27824adf62be4fece8ea5834e548
TJsohn <sohntaejeong@gmail.com>
2025-06-10 20:39:28 +0300

    complete day 23

--- database/migrations/2025_05_19_071922_create_employers_table.php
+++ database/migrations/2025_05_19_071922_create_employers_table.php
@@ -1,28 +1,29 @@
-<?php
+ <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    return new class extends Migration
     {
-        Schema::create('employers', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->timestamps();
-        });
-    }
+        /**
+         * Run the migrations.
+         */
+        public function up(): void
+        {
+            Schema::create('employers', function (Blueprint $table) {
+                $table->id();
+                $table->foreignIdFor(\App\Models\User::class);
+                $table->string('name');
+                $table->timestamps();
+            });
+        }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('employers');
-    }
-};
+        /**
+         * Reverse the migrations.
+         */
+        public function down(): void
+        {
+            Schema::dropIfExists('employers');
+        }
+    };
